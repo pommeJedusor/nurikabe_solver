b474c42e56c7053eaf02fe19fcb9b5f93039de0e
pommeJedusor <118850425+pommeJedusor@users.noreply.github.com>
2026-08-10 15:07:07 +0200

    changed emplacment of echo filename so that if one takes time it's more obvious which one it is

--- test.sh
+++ test.sh
@@ -10,12 +10,12 @@ TMP_DIR=$(mktemp -d)
 for FILE in solutions/*;
 do
 	FILENAME=$(basename $FILE)
+	echo "${FILENAME}"
 	if [ "$CHECK_FOR_MEMORY_LEAKS" = true ] ; then
 		valgrind -q --error-exitcode=1 --exit-on-first-error=yes --leak-check=yes ./nurikabe_solver maps/$FILENAME > $TMP_DIR/$FILENAME
 	else
 		./nurikabe_solver maps/$FILENAME > $TMP_DIR/$FILENAME
 	fi
-	echo "${FILENAME}"
 	diff solutions/$FILENAME $TMP_DIR/$FILENAME
 done
 
